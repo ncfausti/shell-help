a24556acb6f38a27c8543851c826007dfe3ab3ca
Nick Fausti <ncfausti@users.noreply.github.com>
2020-04-11 13:27:08 -0400

    Update curl_login.sh

--- curl_login.sh
+++ curl_login.sh
@@ -4,3 +4,9 @@ curl -X POST "https://piazza.com/class" --data-urlencode "remember=true" \
 --data-urlencode "password=<REPLACE WITH PASSWORD>"     \
 --cookie "cookies.txt"     --cookie-jar "cookies.txt"     \
 --location     --verbose     > "login_log.html"
+
+# add for subsequent requests use --cookie and --cookie-har from last request
+curl "https://piazza.com/class" \
+    --cookie "cookies.txt" \
+    --cookie-jar "cookies.txt" \
+    --head
